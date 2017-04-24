d4b33385f5950584c540140f57ee1a17c243692c
UnstoppableMango <ecr.referee@hotmail.com>
2017-04-23 21:15:13 -0500

    Framework and reference changes

---
+++ Safir.Manager/IO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Safir.Manager
+{
+    internal class IO
+    {
+        public Song Load(Uri file)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void Save(Song song)
+        {
+
+        }
+    }
+}

8e3aec07aa6c9633f8f2d89e6afb123580c72d9d
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-09 00:04:20 -0500

    WIP on initial nav

--- src/Safir.Electron/src/app/components/nav/nav.component.ts
+++ src/Safir.Electron/src/app/components/nav/nav.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
 })
-export class AppComponent {
-
-  title = 'Safir';
+export class NavComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(

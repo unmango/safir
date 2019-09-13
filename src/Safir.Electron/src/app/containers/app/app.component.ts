2c9a88bd1f21907da70d2b9866fad15a2757e56c
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-13 12:28:09 -0500

    Revert: WIP on initial nav

--- src/Safir.Electron/src/app/containers/app/app.component.ts
+++ src/Safir.Electron/src/app/containers/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +6,5 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   title = 'Safir';
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
 }

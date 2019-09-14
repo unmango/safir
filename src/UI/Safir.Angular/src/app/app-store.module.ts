d542f2d43351db63c412b2713de5ccf31dcc4e0b
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-14 02:14:02 -0500

    Add @ngrx/router-store

--- src/UI/Safir.Angular/src/app/app-store.module.ts
+++ src/UI/Safir.Angular/src/app/app-store.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
@@ -20,12 +21,14 @@ import { AppEffects } from './effects';
       maxAge: 25,
       logOnly: environment.production
     }),
-    EffectsModule.forRoot([AppEffects])
+    EffectsModule.forRoot([AppEffects]),
+    StoreRouterConnectingModule.forRoot()
   ],
   exports: [
     StoreModule,
     StoreDevtoolsModule,
-    EffectsModule
+    EffectsModule,
+    StoreRouterConnectingModule
   ]
 })
 export class AppStoreModule { }

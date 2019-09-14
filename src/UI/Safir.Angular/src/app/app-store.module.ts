04a9bf8c7a64df5399efed63a4fb700942179fcb
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-14 00:54:15 -0500

    Add @ngrx/effects

--- src/UI/Safir.Angular/src/app/app-store.module.ts
+++ src/UI/Safir.Angular/src/app/app-store.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
 import { reducers, metaReducers } from './reducers';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   imports: [
@@ -17,11 +19,13 @@ import { reducers, metaReducers } from './reducers';
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production
-    })
+    }),
+    EffectsModule.forRoot([AppEffects])
   ],
   exports: [
     StoreModule,
-    StoreDevtoolsModule
+    StoreDevtoolsModule,
+    EffectsModule
   ]
 })
 export class AppStoreModule { }

3fbe81765d4f3c3429954279f1ff5d2c8e3588cb
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-14 12:29:45 -0500

    Add ngrx logger

--- src/UI/Safir.Angular/src/app/reducers/index.ts
+++ src/UI/Safir.Angular/src/app/reducers/index.ts
@@ -3,7 +3,8 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  Action
 } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
@@ -17,7 +18,19 @@ export const reducers: ActionReducerMap<State> = {
   [fromNav.navFeatureKey]: fromNav.reducer,
 };
 
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return (state: State, action: Action): State => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('prev state', state);
+    console.log('action', action);
+    console.log('next state', result);
+    console.groupEnd();
+
+    return result;
+  };
+}
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
+  ? [logger]
   : [];

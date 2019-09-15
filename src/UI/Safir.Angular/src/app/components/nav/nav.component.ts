5d13d082ddd42e725092b278cb2a4ed1e568197b
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-15 17:30:07 -0500

    WIP CSS is absolute garbage

--- src/UI/Safir.Angular/src/app/components/nav/nav.component.ts
+++ src/UI/Safir.Angular/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { NavItem } from '@app/models';
 
@@ -6,7 +7,39 @@ import { NavItem } from '@app/models';
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('collapsed', style({
+        // transform: 'translate3d(0,0,0)'
+        width: '*'
+      })),
+      state('opened', style({
+        // transform: 'translate3d(100%,0,0)'
+        width: '200px'
+      })),
+      transition('collapsed => opened', animate('400ms ease-in-out')),
+      transition('opened => collapsed', animate('400ms ease-in-out'))
+    ]),
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }),
+        animate(400, style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate(400, style({
+          transform: 'translateX(0)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class NavComponent {
 
@@ -23,6 +56,10 @@ export class NavComponent {
 
   public get opened(): boolean {
     return !this.collapsed;
+  }
+
+  public get state(): string {
+    return this.collapsed ? 'collapsed' : 'opened';
   }
 
   public toggle(): void {

bbeb958dc885d9fed91d80dabd4bd5b0c68f3456
UnstoppableMango <ecr.referee@hotmail.com>
2019-09-14 23:45:25 -0500

    Init angular app (#9)
    * Fix rider BS
    
    * Run ng new
    
    * Install electron as dev dep
    
    * Get electron running
    
    * Add vscode UI workspace
    
    * Basic hot reload
    
    * Add app as workspace folder
    
    * Add @ngrx/store
    
    * Add material packages
    
    * Add shared material module for all imports
    
    * Remove root from electron editorconfig
    
    * Add rider code style config file
    
    * Import material module
    
    * Move store to separate module
    
    * Add component index
    
    * Add container index
    
    * Move app component to containers
    
    * declare components as well
    
    * Add home component
    
    * WIP on initial nav
    
    * Revert: WIP on initial nav
    
    * WIP on infra
    
    * Move leftover files from old dir
    
    * Fix app component merge conflict
    
    * Add back angular material
    
    * Remove electron reload
    
    * Add back @ngrx/store
    
    * Add store devtools
    
    * Add @ngrx/effects
    
    * Move effects to folder
    
    * Add @ngrx/router-store
    
    * Add @ngrx/schematics
    
    * Add @ngrx/entity
    
    * WIP on nav state
    
    * Change mode to boolean
    
    * Add ngrx logger
    
    * WIP on state
    
    * More or less basic app init done

---
+++ src/UI/Safir.Angular/src/app/reducers/index.ts
@@ -0,0 +1,58 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+  Action
+} from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+
+import { environment } from '../../environments/environment';
+import * as fromNav from './nav.reducer';
+
+export interface State {
+  router: fromRouter.RouterReducerState<any>;
+  [fromNav.navFeatureKey]: fromNav.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  router: fromRouter.routerReducer,
+  [fromNav.navFeatureKey]: fromNav.reducer,
+};
+
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return (state: State, action: Action): State => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('prev state', state);
+    console.log('action', action);
+    console.log('next state', result);
+    console.groupEnd();
+
+    return result;
+  };
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? [logger]
+  : [];
+
+
+// Router
+
+export const selectRouter = createFeatureSelector<State, fromRouter.RouterReducerState<any>>('router');
+export const {
+  selectQueryParams,    // select the current route query params
+  selectQueryParam,     // factory function to select a query param
+  selectRouteParams,    // select the current route params
+  selectRouteParam,     // factory function to select a route param
+  selectRouteData,      // select the current route data
+  selectUrl,            // select the current url
+} = fromRouter.getSelectors(selectRouter);
+
+// Nav
+
+export const selectNav = createFeatureSelector<State, fromNav.State>(fromNav.navFeatureKey);
+export const selectNavCollapsed = createSelector(selectNav, state => state.collapsed);
+export const selectNavItems = createSelector(selectNav, state => state.items);

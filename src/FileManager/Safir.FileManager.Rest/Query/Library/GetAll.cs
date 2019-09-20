b4d3732d2f620f41c12543dbc3830cb7d10f3c81
Erik Rasmussen <ecr.referee@hotmail.com>
2019-09-19 21:45:40 -0500

    Add getall query type

---
+++ src/FileManager/Safir.FileManager.Rest/Query/Library/GetAll.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Safir.FileManager.Rest.Query.Library
+{
+    public class GetAll
+    {
+    }
+}

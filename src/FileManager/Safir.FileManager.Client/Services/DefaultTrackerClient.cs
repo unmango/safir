f827d571ea144540d4f085fc66dc1694c07a2e2c
UnstoppableMango <ecr.referee@hotmail.com>
2019-12-19 21:33:12 -0600

    Make the service one again (#47)
    * Remove modular projects and add singular service
    
    * Add some grpc bullshit

---
+++ src/FileManager/Safir.FileManager.Client/Services/DefaultTrackerClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Safir.FileManager.Client.Services
+{
+    internal class DefaultTrackerClient : Tracker.TrackerClient, ITrackerClient
+    {
+        public async Task<TrackResponse> TrackAsync(TrackRequest request, CancellationToken cancellationToken = default)
+        {
+            return await TrackAsync(request, headers: null, deadline: null, cancellationToken: cancellationToken);
+        }
+    }
+}

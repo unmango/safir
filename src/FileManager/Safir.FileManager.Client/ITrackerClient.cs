6ec3b4f1c8b681f0aed0659f1ed20bf32c2aad20
Erik Rasmussen <ecr.referee@hotmail.com>
2019-12-19 21:34:57 -0600

    Add some grpc bullshit

---
+++ src/FileManager/Safir.FileManager.Client/ITrackerClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Safir.FileManager.Client
+{
+    public interface ITrackerClient
+    {
+        Task<TrackResponse> TrackAsync(TrackRequest request, CancellationToken cancellationToken = default);
+    }
+}
--- src/FileManager/Safir.FileManager.Service/Startup.cs
+++ src/FileManager/Safir.FileManager.Service/Startup.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Safir.FileManager.Service.Services;
 
 namespace Safir.FileManager.Service
 {
@@ -16,6 +13,7 @@ namespace Safir.FileManager.Service
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGrpc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +28,7 @@ namespace Safir.FileManager.Service
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGrpcServices();
             });
         }
     }

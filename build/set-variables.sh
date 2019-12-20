4abe89210f3fc779ab77f379e3afee552da9ddf0
Erik Rasmussen <ecr.referee@hotmail.com>
2019-12-19 23:11:16 -0600

    Add script to set build variables for pipeline

---
+++ build/set-variables.sh
@@ -0,0 +1,7 @@
+echo $SOURCE
+echo $TARGET
+echo $PROJECTPATH
+if [[ $(git diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH) ]]; then
+  echo "##vso[task.setvariable variable=BuildFileManager;isOutput=true]true"
+  echo "##vso[task.setvariable variable=FileManagerPath;isOutput=true]$PROJECTPATH"
+fi

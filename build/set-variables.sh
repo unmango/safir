52ae61ea6a5454d80f275d04d5eb1f4b37b2776b
Erik Rasmussen <ecr.referee@hotmail.com>
2019-12-23 08:57:27 -0600

    More trying shit

--- build/set-variables.sh
+++ build/set-variables.sh
@@ -11,9 +11,13 @@ echo $GITPATH
 $GITPATH diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH
 $($GITPATH diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH)
 echo $($GITPATH diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH)
+TMP=$($GITPATH diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH)
+echo $TMP
 if [[ $($GITPATH diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH) ]]; then
   echo "##vso[task.setvariable variable=BuildFileManager;isOutput=true]true"
   echo "##vso[task.setvariable variable=FileManagerPath;isOutput=true]$PROJECTPATH"
+  echo "True"
 else
   echo "##vso[task.setvariable variable=BuildFileManager;isOutput=true]false"
+  echo "False"
 fi

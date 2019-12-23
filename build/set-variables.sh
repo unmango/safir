94acd99cde64d6384ad02720d77a7283ddba11fb
Erik Rasmussen <ecr.referee@hotmail.com>
2019-12-22 22:46:06 -0600

    Trying shit again

--- build/set-variables.sh
+++ build/set-variables.sh
@@ -6,6 +6,9 @@ PROJECTPATH="$ROOTPATH/FileManager"
 echo $SOURCE
 echo $TARGET
 echo $PROJECTPATH
+GITPATH=$(which git)
+echo $GITPATH
+git diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH
 if [[ $(git diff-tree --dirstat $SOURCE..$TARGET -- $PROJECTPATH) ]]; then
   echo "##vso[task.setvariable variable=BuildFileManager;isOutput=true]true"
   echo "##vso[task.setvariable variable=FileManagerPath;isOutput=true]$PROJECTPATH"

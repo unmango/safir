ee8dd883c8b7896c6a359c9c07684fa5731615fe
UnstoppableMango <ecr.referee@hotmail.com>
2020-02-06 18:52:46 -0600

    Just merge in what I got (#112)
    * Rider BS
    
    * WIP on the CLI app framework
    
    * Add CLI projects back into solution
    
    * Update packages for 3.0
    
    * Removed an empty line
    
    * WIP on how I want to configure the CLI app
    
    * More WIP
    
    * Today was one of those random garbage days
    
    * Update dep config and dotnet version
    
    * Add rider files
    
    * Update commandline packages
    
    * Remove lowercase .idea folder I guess
    
    * Update packages
    
    * WIP on random shit
    
    * Commit the BS so it stops showing up
    
    * Auto stash before rebase of "origin/feature/cli"
    
    * Use latestMajor rollForward policy
    
    * Move CLI projects around
    
    * FIx build issues
    
    * Random library garbage
    
    * Some more garbage
    
    * Add new projects for dependabot
    
    * Move cli projects and add dedicated sln
    
    * Init more vscode shit
    
    * Move templates back under build
    
    * Automerge CLI packages

---
+++ src/Cli/Safir.Cli/Commands/Add/FileHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Safir.Cli.Commands.Add
+{
+    internal class FileHandler
+    {
+    }
+}
---
+++ src/Cli/Safir.CommandLine/CommandLineBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.Text;
+
+namespace Safir.CommandLine
+{
+    public static class CommandLineBuilderExtensions
+    {
+    }
+}

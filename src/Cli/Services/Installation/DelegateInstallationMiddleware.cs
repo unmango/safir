b442f80a5983707e00447e2e1db96828911fef21
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-09 17:23:04 -0600

    Inherit installation middleware from behaviour

--- src/Cli/Services/Installation/DelegateInstallationMiddleware.cs
+++ src/Cli/Services/Installation/DelegateInstallationMiddleware.cs
@@ -1,29 +1,13 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Cli.Internal.Pipeline;
 
 namespace Cli.Services.Installation
 {
-    internal class DelegateInstallationMiddleware : IInstallationMiddleware
+    internal class DelegateInstallationMiddleware : DelegateBehaviour<InstallationContext>, IInstallationMiddleware
     {
-        private readonly AppliesTo<InstallationContext> _appliesTo;
-        private readonly InvokeAsync<InstallationContext> _invokeAsync;
-
         public DelegateInstallationMiddleware(
             AppliesTo<InstallationContext> appliesTo,
             InvokeAsync<InstallationContext> invokeAsync)
-        {
-            _appliesTo = appliesTo ?? throw new ArgumentNullException(nameof(appliesTo));
-            _invokeAsync = invokeAsync ?? throw new ArgumentNullException(nameof(invokeAsync));
-        }
-
-        public bool AppliesTo(InstallationContext context) => _appliesTo(context);
-
-        public ValueTask InvokeAsync(
-            InstallationContext context,
-            Func<InstallationContext, ValueTask> next,
-            CancellationToken cancellationToken = default)
-            => _invokeAsync(context, next, cancellationToken);
+            : base(appliesTo, invokeAsync)
+        { }
     }
 }

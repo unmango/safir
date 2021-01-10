bbf894dd735fc78ca5998bdf4c768aa557ea7588
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-09 23:51:00 -0600

    Update delegate behaviour to allow setting the applies to result

--- src/Cli/Internal/Pipeline/DelegateBehaviour.cs
+++ src/Cli/Internal/Pipeline/DelegateBehaviour.cs
@@ -7,11 +7,14 @@ namespace Cli.Internal.Pipeline
     internal class DelegateBehaviour<T> : IPipelineBehaviour<T>
         where T : class
     {
+        private readonly bool _appliesToResult;
         private readonly AppliesTo<T>? _appliesTo;
         private readonly InvokeAsync<T>? _invokeAsync;
 
-        public DelegateBehaviour(InvokeAsync<T> invokeAsync) : this(null, invokeAsync)
+        public DelegateBehaviour(InvokeAsync<T> invokeAsync, bool appliesToResult = false)
+            : this(null, invokeAsync)
         {
+            _appliesToResult = appliesToResult;
         }
         
         public DelegateBehaviour(AppliesTo<T>? appliesTo, InvokeAsync<T>? invokeAsync)
@@ -22,7 +25,7 @@ namespace Cli.Internal.Pipeline
         
         public bool AppliesTo(T context)
         {
-            return _appliesTo?.Invoke(context) ?? false;
+            return _appliesTo?.Invoke(context) ?? _appliesToResult;
         }
 
         public ValueTask InvokeAsync(T context, Func<T, ValueTask> next, CancellationToken cancellationToken = default)

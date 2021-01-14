698be775444dc92ce3ffd5a73e2fbd1efb39da45
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-13 21:22:03 -0600

    WIP add default service status impl

---
+++ src/Cli/Services/DefaultServiceStatus.cs
@@ -0,0 +1,10 @@
+namespace Cli.Services
+{
+    internal class DefaultServiceStatus : IServiceStatus
+    {
+        public ServiceStatus GetStatus(IService service)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

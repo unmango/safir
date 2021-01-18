91806ebf26dba4bd40946e701dc14223cbb84eb1
UnstoppableMango <ecr.referee@hotmail.com>
2021-01-18 00:10:42 -0600

    Fix using deprecated constructor (#23)

--- src/Cli/Internal/Validation/UrlValidator.cs
+++ src/Cli/Internal/Validation/UrlValidator.cs
@@ -5,9 +5,11 @@ namespace Cli.Internal.Validation
 {
     public class UrlValidator : PropertyValidator
     {
-        public UrlValidator() : base("{PropertyName} must be a valid url.")
-        { }
-        
+        protected override string GetDefaultMessageTemplate()
+        {
+            return "{PropertyName} must be a valid url.";
+        }
+
         protected override bool IsValid(PropertyValidatorContext context)
         {
             return context.PropertyValue is string value

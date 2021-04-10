cb2996d6b298e0051bb376c628b9b1a7735da208
Erik Rasmussen <ecr.referee@hotmail.com>
2021-04-10 10:46:58 -0500

    Remove file provider abstractions

--- src/Safir.Agent/Services/IFileProviderFactory.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.FileProviders;
-
-namespace Safir.Agent.Services
-{
-    public interface IFileProviderFactory
-    {
-        IFileProvider Create(string path);
-    }
-}

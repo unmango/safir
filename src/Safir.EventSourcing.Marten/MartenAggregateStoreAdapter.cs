ae0ee313f320c76682b28a0630b4f02b77864781
Erik Rasmussen <ecr.referee@hotmail.com>
2021-06-06 10:23:16 -0500

    Fix class todo

--- src/Safir.EventSourcing.Marten/MartenAggregateStoreAdapter.cs
+++ src/Safir.EventSourcing.Marten/MartenAggregateStoreAdapter.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Marten;
+using Microsoft.Extensions.Logging;
 
 namespace Safir.EventSourcing.Marten
 {
@@ -11,41 +12,53 @@ namespace Safir.EventSourcing.Marten
     {
         private readonly IDocumentStore _store;
         private readonly IEventStore _eventStore;
+        private readonly ILogger<MartenAggregateStoreAdapter> _logger;
 
-        public MartenAggregateStoreAdapter(IDocumentStore store, IEventStore eventStore)
+        public MartenAggregateStoreAdapter(
+            IDocumentStore store,
+            IEventStore eventStore,
+            ILogger<MartenAggregateStoreAdapter> logger)
         {
             _store = store ?? throw new ArgumentNullException(nameof(store));
             _eventStore = eventStore ?? throw new ArgumentNullException(nameof(eventStore));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public Task StoreAsync<T>(T aggregate, CancellationToken cancellationToken = default) where T : IAggregate
+        public Task StoreAsync<T>(T aggregate, CancellationToken cancellationToken = default)
+            where T : class, IAggregate
         {
+            _logger.LogTrace("Dequeuing events from aggregate");
             var events = aggregate.DequeueEvents().ToList();
 
             if (!events.Any()) return Task.CompletedTask;
 
+            _logger.LogTrace("Adding events to event store");
             return _eventStore.AddAsync(aggregate.Id, events, cancellationToken);
         }
 
         public ValueTask<T> GetAsync<T>(long id, CancellationToken cancellationToken = default)
-            where T : IAggregate, new()
+            where T : class, IAggregate, new()
         {
-            // TODO: State needs to be class, what do?
-            // using var session = _store.OpenSession();
-            // var task = session.Events.AggregateStreamAsync(
-            //     id.ToString(),
-            //     state: new T(),
-            //     token: cancellationToken);
-            //
-            // return new ValueTask<T>(task);
-
-            throw new NotImplementedException();
+            using var session = _store.OpenSession();
+            var task = session.Events.AggregateStreamAsync(
+                id.ToString(),
+                state: new T(),
+                token: cancellationToken);
+            
+            return new ValueTask<T>(task);
         }
 
         public ValueTask<T> GetAsync<T>(long id, int version, CancellationToken cancellationToken = default)
-            where T : IAggregate, new()
+            where T : class, IAggregate, new()
         {
-            throw new System.NotImplementedException();
+            using var session = _store.OpenSession();
+            var task = session.Events.AggregateStreamAsync(
+                id.ToString(),
+                version,
+                state: new T(),
+                token: cancellationToken);
+            
+            return new ValueTask<T>(task);
         }
     }
 }

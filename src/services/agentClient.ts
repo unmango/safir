b8adb8c2f34cc2103faa8b0ae8ef89731ebd5f8d
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-01 16:07:32 -0500

    Better dotenv

--- src/services/agentClient.ts
+++ src/services/agentClient.ts
@@ -1,13 +1,8 @@
 import { fileSystem } from '@unmango/safir-agent-client/dist/clients';
 
-const baseUrl = process.env.AGENT_URL ?? '';
+const baseUrl = process.env.REACT_APP_AGENT_URL ?? '';
 
-const {
+export const {
     list,
     listAsync,
 } = fileSystem.createClient(baseUrl);
-
-export {
-    list,
-    listAsync,
-};

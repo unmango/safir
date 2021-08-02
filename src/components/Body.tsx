61a08bf36b2ce9734073ad8aa1f1c6e965bbf2a1
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-01 19:31:52 -0500

    WIP maybe list files idk its failing

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { list } from '../services/agentClient';
+
 const Body: React.FC = () => {
-    return (
-        <span>Body</span>
+  const [files, setFiles] = useState<string[]>([]);
+
+  list().subscribe((x) => console.log(x));
+
+  useEffect(() => {
+    console.log('in effect');
+    const subscription = list().subscribe(
+      (x) => {
+        console.log('in next');
+        setFiles((f) => [...f, x]);
+      },
+      (e) => console.error(e),
+      () => console.log('completed')
     );
+    return () => {
+      console.log('unsubscribing');
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div>
+      <span>Body</span>
+      {files.map((file) => (
+        <span>File: {file}</span>
+      ))}
+    </div>
+  );
 };
 
 export default Body;

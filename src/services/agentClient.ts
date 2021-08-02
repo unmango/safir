3310156ec5d9bca6c1c6de61507b8920a10bb54a
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-01 19:31:34 -0500

    Indentation

--- src/services/agentClient.ts
+++ src/services/agentClient.ts
@@ -3,6 +3,6 @@ import { fileSystem } from '@unmango/safir-agent-client/dist/clients';
 const baseUrl = process.env.REACT_APP_AGENT_URL ?? '';
 
 export const {
-    list,
-    listAsync,
+  list,
+  listAsync,
 } = fileSystem.createClient(baseUrl);

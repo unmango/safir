e17741f2eabc8a909657498641bb351d34e80a29
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-03 23:20:49 -0500

    Log the baseurl just to be sure I know what it is

--- src/services/agentClient.ts
+++ src/services/agentClient.ts
@@ -1,6 +1,7 @@
 import { fileSystem } from '@unmango/safir-agent-client/dist/clients';
 
 const baseUrl = process.env.REACT_APP_AGENT_URL ?? '';
+console.log('baseUrl', baseUrl);
 
 export const {
   list,

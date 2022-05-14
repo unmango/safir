c92d0167762885f2ae6ea92fa5489eca9b0201d4
Erik Rasmussen <ecr.referee@hotmail.com>
2022-05-14 15:56:06 -0500

    Refactor add command

--- src/cli/src/Safir.Cli/Commands/Config/AddCommand.cs
+++ src/cli/src/Safir.Cli/Commands/Config/AddCommand.cs
@@ -1,24 +1,24 @@
-using System;
 using System.CommandLine;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Safir.Cli.DependencyInjection;
 
 namespace Safir.Cli.Commands.Config;
 
 internal static class AddCommand
 {
+    private static readonly IBinderFactory _services = new ServiceCollection()
+        .AddSafirCliCore()
+        .BuildBinderFactory();
+
     public static Command Create()
     {
         var command = new Command("add", "Add a Safir service to be used with the CLI");
 
-        command.SetHandler(CreateHandler());
+        command.SetHandler<IConsole>(Handle);
 
         return command;
     }
 
-    internal static Func<Task> CreateHandler()
-    {
-        return Handle;
-    }
-
-    internal static async Task Handle() { }
+    internal static async Task Handle(IConsole console) { }
 }

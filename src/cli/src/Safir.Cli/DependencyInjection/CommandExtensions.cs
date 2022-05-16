81548cabb3bffe81a8a70acc3391c6e429fbd580
Erik Rasmussen <ecr.referee@hotmail.com>
2022-05-15 21:55:23 -0500

    Add extensions for setting a handler with a service provider

---
+++ src/cli/src/Safir.Cli/DependencyInjection/CommandExtensions.cs
@@ -0,0 +1,63 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Safir.Cli.DependencyInjection;
+
+internal static class CommandExtensions
+{
+    public static void SetHandler<T>(this Command command, Action<T, ParseResult> execute, IServiceCollection services)
+        where T : class
+    {
+        command.SetHandler(execute, services.CreateBinder<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, int> execute, IServiceCollection services)
+        where T : class
+    {
+        command.SetHandler(
+            (T handler, InvocationContext context) => context.ExitCode = execute(handler, context.ParseResult),
+            services.CreateBinder<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, Task> execute, IServiceCollection services)
+        where T : class
+    {
+        command.SetHandler(execute, services.CreateBinder<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, Task<int>> execute, IServiceCollection services)
+        where T : class
+    {
+        command.SetHandler(execute, services.CreateBinder<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Action<T, ParseResult> execute, IBinderFactory services)
+        where T : class
+    {
+        command.SetHandler(execute, services.Create<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, int> execute, IBinderFactory services)
+        where T : class
+    {
+        command.SetHandler(
+            (T handler, InvocationContext context) => context.ExitCode = execute(handler, context.ParseResult),
+            services.Create<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, Task> execute, IBinderFactory services)
+        where T : class
+    {
+        command.SetHandler(execute, services.Create<T>());
+    }
+
+    public static void SetHandler<T>(this Command command, Func<T, ParseResult, Task<int>> execute, IBinderFactory services)
+        where T : class
+    {
+        command.SetHandler(execute, services.Create<T>());
+    }
+}

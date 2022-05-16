7057576c28f136593fddce38b209e1d8b5066e84
Erik Rasmussen <ecr.referee@hotmail.com>
2022-05-15 21:55:07 -0500

    Refactor binder factory stuff

--- src/cli/src/Safir.Cli/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/cli/src/Safir.Cli/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System.CommandLine;
+using System.CommandLine.Binding;
 using System.CommandLine.IO;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,5 +16,26 @@ internal static class ServiceCollectionExtensions
         return services;
     }
 
-    public static ServiceProviderBinderFactory BuildBinderFactory(this IServiceCollection services) => new(services);
+    public static BinderBase<T> CreateBinder<T>(this IServiceCollection services)
+        where T : notnull
+        => new ServiceProviderBinder<T>(services);
+
+    public static IBinderFactory BuildBinderFactory(this IServiceCollection services)
+        => new ServiceProviderBinderFactory(services);
+
+    private sealed class ServiceProviderBinder<T> : BinderBase<T>
+        where T : notnull
+    {
+        private readonly IServiceCollection _services;
+
+        public ServiceProviderBinder(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        protected override T GetBoundValue(BindingContext bindingContext)
+        {
+            return _services.BuildServiceProvider().GetRequiredService<T>();
+        }
+    }
 }

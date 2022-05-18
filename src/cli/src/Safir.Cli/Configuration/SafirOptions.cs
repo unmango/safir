73bc17bd80b4f3059bd414828e55a2ec43b8a4fb
Erik Rasmussen <ecr.referee@hotmail.com>
2022-05-17 23:24:34 -0500

    WIP trying out agent options

--- src/cli/src/Safir.Cli/Configuration/SafirOptions.cs
+++ src/cli/src/Safir.Cli/Configuration/SafirOptions.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using System.IO;
 
 namespace Safir.Cli.Configuration;
 
 internal record SafirOptions
 {
+    public ICollection<AgentOptions> Agents { get; } = new List<AgentOptions>();
+
     public ConfigOptions Config { get; init; } = new();
+}
+
+internal record AgentOptions
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Uri { get; set; } = string.Empty;
 }
 
 internal record ConfigOptions

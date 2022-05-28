0726302ca69a3617357527f889a8bf0945f3fb4f
Erik Rasmussen <ecr.referee@hotmail.com>
2022-05-27 23:13:53 -0500

    WIP adding tests

---
+++ src/cli/test/Safir.CommandLine.Tests/CommandExtensionsTests.cs
@@ -0,0 +1,26 @@
+using System.CommandLine;
+using Moq;
+
+namespace Safir.CommandLine.Tests;
+
+public class CommandExtensionsTests
+{
+    private readonly Mock<IHandlerBuilder> _builder = new();
+    private readonly Command _command = new("test");
+
+    [Fact]
+    public void CommandSetHandler_BuildsHandler()
+    {
+        _command.SetHandler(_builder.Object);
+
+        _builder.Verify(x => x.Build());
+    }
+
+    [Fact]
+    public void BuilderSetHandler_BuildsHandler()
+    {
+        _builder.Object.SetHandler(_command);
+
+        _builder.Verify(x => x.Build());
+    }
+}

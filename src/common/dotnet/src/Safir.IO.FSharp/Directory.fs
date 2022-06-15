6dbc1468271a52653faf402b162a6f9db3eb2a9f
Erik Rasmussen <ecr.referee@hotmail.com>
2022-06-14 21:48:42 -0500

    WIP maybe implementing reader functions

--- src/common/dotnet/src/Safir.IO.FSharp/Directory.fs
+++ src/common/dotnet/src/Safir.IO.FSharp/Directory.fs
@@ -1,6 +1,9 @@
 module Safir.IO.FSharp.Directory
 
 open System.IO
+open System.IO.Abstractions
+
+type DirectoryAction<'a> = DirectoryAction of (IDirectory -> 'a)
 
 let enumerateFileSystemEntries =
     Directory.EnumerateFileSystemEntries

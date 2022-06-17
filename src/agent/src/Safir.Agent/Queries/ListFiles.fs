e0db934e19500b4d6cfed5dd8b140ed662188765
Erik Rasmussen <ecr.referee@hotmail.com>
2022-06-16 21:29:10 -0500

    WIP don't use reader monad

--- src/agent/src/Safir.Agent/Queries/ListFiles.fs
+++ src/agent/src/Safir.Agent/Queries/ListFiles.fs
@@ -1,28 +1,10 @@
 module Safir.Agent.Queries.ListFiles
 
-open System.IO.Abstractions
-open Microsoft.Extensions.Options
+open System.IO
 open Safir.Agent.Configuration
 open Safir.Agent.Protos
-open Safir.Common.Reader
-open Safir.IO.FSharp.Path
 
-let listFiles =
-    reader {
-        let! (options: IOptions<AgentOptions>), (directory: IDirectory), (path: IPath) = ask
-
-        let getDataDirectory =
-            DataDirectory.get |> run directory
-
-        let enumerate (DataDirectory x) =
-            directory.EnumerateFileSystemEntries(x, "*")
-            |> Seq.map (path.getRelativePath x)
-            |> Seq.map (fun f -> FileSystemEntry(Path = f))
-
-        return
-            options.Value.DataDirectory
-            |> (getDataDirectory >> Result.map enumerate)
-    }
-
-type ListFilesWrapper(options, directory, path) =
-    member _.run = run (options, directory, path) listFiles
+let listFiles (DataDirectory dataDirectory) maxDepth enumerateFileSystemEntries getRelativePath =
+    enumerateFileSystemEntries(dataDirectory, "*", EnumerationOptions(MaxRecursionDepth = maxDepth))
+    |> Seq.map (getRelativePath dataDirectory)
+    |> Seq.map (fun f -> FileSystemEntry(Path = f))

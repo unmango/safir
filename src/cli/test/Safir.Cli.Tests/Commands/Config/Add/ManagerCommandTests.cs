45920bb966b71a43ee9b90b71e9e7f796c83ef6c
Erik Rasmussen <ecr.referee@hotmail.com>
2022-10-12 20:29:36 -0500

    Unit tests

--- src/cli/test/Safir.Cli.Tests/Commands/Config/Add/ManagerCommandTests.cs
+++ src/cli/test/Safir.Cli.Tests/Commands/Config/Add/ManagerCommandTests.cs
@@ -3,27 +3,28 @@ using System.CommandLine.IO;
 using Microsoft.Extensions.Options;
 using Moq;
 using Safir.Cli.Commands.Config;
+using Safir.Cli.Commands.Config.Add;
 using Safir.Cli.Configuration;
 using Xunit;
 
-namespace Safir.Cli.Tests.Commands.Config;
+namespace Safir.Cli.Tests.Commands.Config.Add;
 
-public class AddCommandTests
+public class ManagerCommandTests
 {
     private readonly Mock<IStandardStreamWriter> _outWriter = new();
     private readonly Mock<IConsole> _console = new();
-    private readonly Mock<IOptionsMonitor<SafirOptions>> _options = new();
+    private readonly Mock<IOptions<SafirOptions>> _options = new();
     private readonly Mock<IUserConfiguration> _configuration = new();
-    private readonly AddCommand.AddCommandHandler _handler;
+    private readonly ManagerCommand.Handler _handler;
 
     private readonly SafirOptions _defaultOptions = new() {
-        Agents = new List<AgentOptions>(),
+        Managers = new List<ManagerOptions>(),
     };
 
-    public AddCommandTests()
+    public ManagerCommandTests()
     {
         _console.SetupGet(x => x.Out).Returns(_outWriter.Object);
-        _options.SetupGet(x => x.CurrentValue).Returns(_defaultOptions);
+        _options.SetupGet(x => x.Value).Returns(_defaultOptions);
 
         _handler = new(_console.Object, _options.Object, _configuration.Object);
     }
@@ -31,7 +32,7 @@ public class AddCommandTests
     [Fact]
     public void Value_RequiresServiceArgument()
     {
-        var command = AddCommand.Value;
+        var command = ManagerCommand.Value;
 
         Assert.Contains(AddCommand.ServiceArgument, command.Arguments);
     }
@@ -39,7 +40,7 @@ public class AddCommandTests
     [Fact]
     public void Value_RequiresUriArgument()
     {
-        var command = AddCommand.Value;
+        var command = ManagerCommand.Value;
 
         Assert.Contains(AddCommand.UriArgument, command.Arguments);
     }
@@ -47,7 +48,7 @@ public class AddCommandTests
     [Fact]
     public void Value_ValidatesUriArgument()
     {
-        var command = AddCommand.Value;
+        var command = ManagerCommand.Value;
 
         var result = command.Parse("test not-a-uri");
 
@@ -58,7 +59,7 @@ public class AddCommandTests
     [Fact]
     public void Value_SetsHandler()
     {
-        var command = AddCommand.Value;
+        var command = ManagerCommand.Value;
 
         Assert.NotNull(command.Handler);
     }
@@ -68,10 +69,10 @@ public class AddCommandTests
     [InlineData("test")]
     public async Task Handler_ShortCircuitsWhenServiceAlreadyExists(string expectedName)
     {
-        var parseResult = AddCommand.Value.Parse($"{expectedName} https://example.com");
-        _options.SetupGet(x => x.CurrentValue).Returns(_defaultOptions with {
-            Agents = new[] {
-                new AgentOptions {
+        var parseResult = ManagerCommand.Value.Parse($"{expectedName} https://example.com");
+        _options.SetupGet(x => x.Value).Returns(_defaultOptions with {
+            Managers = new[] {
+                new ManagerOptions {
                     Name = expectedName.ToLower(),
                 },
             },
@@ -89,7 +90,7 @@ public class AddCommandTests
     {
         const string expectedName = "test";
         const string expectedUri = "https://example.com";
-        var parseResult = AddCommand.Value.Parse($"{expectedName} {expectedUri}");
+        var parseResult = ManagerCommand.Value.Parse($"{expectedName} {expectedUri}");
         Action<LocalConfiguration>? update = null;
         _configuration.Setup(x => x.UpdateAsync(
                 It.IsAny<Action<LocalConfiguration>>(),
@@ -98,9 +99,9 @@ public class AddCommandTests
 
         await _handler.Execute(parseResult);
 
-        var testConfig = new LocalConfiguration(new List<AgentConfiguration>());
+        var testConfig = new LocalConfiguration(new List<AgentConfiguration>(), new List<ManagerConfiguration>());
         update?.Invoke(testConfig);
-        Assert.Contains(testConfig.Agents, x => x is {
+        Assert.Contains(testConfig.Managers, x => x is {
             Name: expectedName, Uri.OriginalString: expectedUri,
         });
         _outWriter.Verify(x => x.Write(It.IsRegex(".*[Aa]dded.*")));

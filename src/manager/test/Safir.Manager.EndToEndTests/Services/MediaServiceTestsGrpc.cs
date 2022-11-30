61a53483d8c36b4aebc4feab6b694791ad5aeb79
Erik Rasmussen <ecr.referee@hotmail.com>
2022-11-30 01:08:44 -0600

    WIP writing e2e test

--- src/manager/test/Safir.Manager.EndToEndTests/Services/MediaServiceTestsGrpc.cs
+++ src/manager/test/Safir.Manager.EndToEndTests/Services/MediaServiceTestsGrpc.cs
@@ -1,4 +1,5 @@
 using Google.Protobuf.WellKnownTypes;
+using Safir.EndToEndTesting;
 using Safir.Grpc;
 using Xunit.Abstractions;
 
@@ -8,18 +9,33 @@ namespace Safir.Manager.EndToEndTests.Services;
 [Trait("Category", "EndToEnd")]
 public class MediaServiceTestsGrpc : ManagerServiceTestBase
 {
+    private const string DataDirectory = "/data";
+
+    private static readonly ConfigureServiceTest _configureTest =
+        configuration => configuration
+            .WithEnvironment("DataDirectory", DataDirectory);
+
     public MediaServiceTestsGrpc(ManagerServiceFixture service, ITestOutputHelper output)
-        : base(service, output) { }
+        : base(service, output, _configureTest) { }
 
     [Fact]
     public async Task List_ReturnsTestData()
     {
+        const string fileName = "Test.txt";
+        var execResult = await Container.ExecAsync(new List<string> {
+            "touch", Path.Combine(DataDirectory, fileName),
+        });
+
+        if (execResult.ExitCode > 0) {
+            Assert.Fail(execResult.Stderr);
+        }
+
         var result = await GetMediaClient().List(new Empty())
             .ResponseStream
             .ToListAsync();
 
         var item = Assert.Single(result);
         Assert.Equal("Test", item.Host);
-        Assert.Equal("Test", item.Path);
+        Assert.Equal(fileName, item.Path);
     }
 }

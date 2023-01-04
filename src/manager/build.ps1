ae45907437ba7e2758d33ebe9471e4a16a7f4571
Erik Rasmussen <ecr.referee@hotmail.com>
2023-01-03 23:03:55 -0600

    Better .dockerignore

--- src/manager/build.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-$ErrorActionPreference = 'Stop'
-
-docker build . --file Dockerfile `
-  --build-arg GithubUsername=$env:GITHUB_USERNAME `
-  --build-arg GithubPassword=$env:GITHUB_PASSWORD `
-  --tag safir-agent:$(date +%s)

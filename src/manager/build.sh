6b61d71725e14fb4fb145dc77f9277cece5694d7
UnstoppableMango <ecr.referee@hotmail.com>
2023-01-04 00:03:17 -0600

    Better .dockerignore (#963)

--- src/manager/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build . --file Dockerfile \
-  --build-arg GithubUsername="$GITHUB_USERNAME" \
-  --build-arg GithubPassword="$GITHUB_PASSWORD" \
-  --tag safir-agent:"$(date +%s)"

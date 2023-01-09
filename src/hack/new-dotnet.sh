2087bb4289895c5d7c815c3250ffaf6eda3837be
Erik Rasmussen <ecr.referee@hotmail.com>
2023-01-08 19:48:07 -0600

    Allow creating test project in hack script

--- src/hack/new-dotnet.sh
+++ src/hack/new-dotnet.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 set -e
 
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-    exit 1
+read -p 'Name: ' NAME
+read -p 'Test? [Y/n] (Default n): ' TEST
+TEST=${TEST:-n}
+
+if [[ "${TEST,,}" == "y" ]]; then
+    TARG="xunit";
+else
+    TARG="classlib";
 fi
 
-if [ -n "$2" ]; then
-    EXTRA_ARGS="-lang $2"
-fi
-
-NAME=$1
+read -p 'Lang? [C#/F#] (Default C#): ' PLANG
+PLANG=${PLANG:-C#}
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 REL_COMMON="src/common/dotnet"
 DIR="$GIT_ROOT/$REL_COMMON/src/$NAME"
 
 mkdir -p "$DIR"
-
 pushd "$DIR"
-
-dotnet new classlib $EXTRA_ARGS
+dotnet new $TARG -lang $PLANG
 
 cd "$GIT_ROOT/$REL_COMMON"
-
 dotnet sln add "$DIR"
 
 cd "$GIT_ROOT"
-
 dotnet sln add -s common "$DIR"
 
 popd

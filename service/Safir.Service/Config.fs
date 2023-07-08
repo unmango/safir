49d14b5205b2f21ca2c9345edb441a72ef1339e4
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-08 18:49:35 -0500

    feat: Add EventStoreDb integration and source module
    This commit adds the necessary code changes to integrate with EventStoreDb. It introduces a new `Propulsion.EventStoreDb` namespace and imports it in the `Config.fs` file. Additionally, it defines a `checkpointInterval` value in the same file.
    
    The `Config.fs` file also includes changes related to the `EventStoreConnector`, which is now defined as a private member within a new `Store` module. The module provides functions for establishing connections and creating event store contexts.
    
    In the `Source` module of `Config.fs`, a new function called `create` is added, which creates an instance of an event store source using various parameters.
    
    In the `Program.fs` file, there are modifications made to the DI (Dependency Injection) module. The registration function now takes only a connection string parameter and registers additional services related to EventStoreDb integration.
    
    Lastly, in the `.fsproj` file, two new files (`CheckpointStore.fs` and `Ingester.fs`) are included in the project.
    
    These changes enable integration with EventStoreDb and set up necessary components for handling events from EventStoreDb.

--- service/Safir.Service/Config.fs
+++ service/Safir.Service/Config.fs
@@ -3,6 +3,9 @@ module Safir.Service.Config
 open System
 open Equinox
 open Equinox.EventStoreDb
+open Propulsion.EventStoreDb
+
+let checkpointInterval = TimeSpan.FromHours 1
 
 let accessStrategy isOrigin toSnapshot =
     AccessStrategy.RollingSnapshots(isOrigin, toSnapshot)
@@ -11,11 +14,30 @@ let category codec fold initial accessStrategy (context, cache) : Category<'a, '
     let cacheStrategy = CachingStrategy.SlidingWindow(cache, TimeSpan.FromMinutes 20)
     EventStoreCategory(context, codec, fold, initial, cacheStrategy, accessStrategy)
 
-let connector = EventStoreConnector(reqTimeout = TimeSpan.FromSeconds(3), reqRetries = 1)
+module Store =
+    let private connector = EventStoreConnector(reqTimeout = TimeSpan.FromSeconds 3, reqRetries = 1)
 
-let connect connectionString =
-    let name = "Test"
-    let strategy = ConnectionStrategy.ClusterTwinPreferSlaveReads
-    let discovery = Discovery.ConnectionString connectionString
-    let connection = connector.Establish(name, discovery, strategy)
-    EventStoreContext(connection, batchSize = 500), Cache(name, sizeMb = 50)
+    let connect name connectionString =
+        let strategy = ConnectionStrategy.ClusterTwinPreferSlaveReads
+        let discovery = Discovery.ConnectionString connectionString
+        connector.Establish(name, discovery, strategy)
+
+    let create name connection =
+        EventStoreContext(connection, batchSize = 500), Cache(name, sizeMb = 50)
+
+module Source =
+    let create log (connection: EventStoreConnection) checkpoints sink filter =
+        let batchSize = 50
+        let statsInterval = TimeSpan.FromMinutes 5
+        let tailSleepInterval = TimeSpan.FromMinutes 5
+
+        EventStoreSource(
+            log,
+            statsInterval,
+            connection.ReadConnection,
+            batchSize,
+            tailSleepInterval,
+            checkpoints,
+            sink,
+            categoryFilter = filter
+        )
--- service/Safir.Service/Program.fs
+++ service/Safir.Service/Program.fs
@@ -16,20 +16,25 @@ Log.Logger <- logger
 module DI =
     open Equinox
 
-    let register connectionString create (services: IServiceCollection) =
+    [<Literal>]
+    let ConnectionName = "Safir"
+
+    let register connectionString (services: IServiceCollection) =
         let resolve = Decider.resolve logger
-        let store = Config.connect connectionString
-        let service: 's = create resolve store
-        services.AddSingleton<'s>(service)
+        let connection = Config.Store.connect ConnectionName connectionString
+        let store = Config.Store.create ConnectionName connection
+        services.AddSingleton(Files.create resolve store)
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
-
     let connectionString = builder.Configuration.GetConnectionString("EventStore")
-    let register = DI.register connectionString
 
-    builder.Services |> register Files.create |> ignore
+    builder.Services
+        .AddHostedService<EventStoreSource>()
+        .AddSingleton(CheckpointStore(logger, Config.checkpointInterval))
+    |> DI.register connectionString
+    |> ignore
 
     builder.Services
         .AddGrpcReflection()

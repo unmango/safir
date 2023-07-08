edb6ea7553e60eb855e903587d17354513b3fff9
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-08 18:48:33 -0500

    Add CheckpointStore implementation for Safir.Service
    This commit adds the implementation of the CheckpointStore module in the Safir.Service namespace. The CheckpointStore module provides functionality for committing and retrieving positions for streams. It uses a ConcurrentDictionary to store the positions and implements the IFeedCheckpointStore interface. The Commit method allows committing a position for a given source, tranche, and cancellationToken. The Start method retrieves the position for a given source, tranche, establishOrigin function, and cancellationToken.

---
+++ service/Safir.Service/CheckpointStore.fs
@@ -0,0 +1,42 @@
+namespace Safir.Service
+
+open System.Collections.Concurrent
+open Propulsion.Feed
+open Serilog
+
+type CheckpointStore(logger: ILogger, defaultCheckpointFrequency) =
+    let store = ConcurrentDictionary<string, Position>()
+
+    let addValue _ position = position
+    let updateValue _ _ position = position
+
+    let streamName source tranche =
+        let source = source |> SourceId.toString |> (fun x -> x.TrimStart('$', '_'))
+        sprintf "%s-%s_%s" source (TrancheId.toString tranche) "checkpoint"
+
+    interface IFeedCheckpointStore with
+        member this.Commit(source, tranche, position, cancellationToken) = task {
+            let stream = streamName source tranche
+            logger.Debug("Committing position for {Stream}: {Position}", stream, position)
+            store.AddOrUpdate(stream, addValue, updateValue, position) |> ignore
+            return ()
+        }
+
+        member this.Start(source, tranche, establishOrigin, cancellationToken) = task {
+            let stream = streamName source tranche
+            let found, position = store.TryGetValue(stream)
+
+            let! position =
+                match found, position, establishOrigin with
+                | true, position, _ ->
+                    logger.Debug("Getting position for stream {Stream}: {Position}", stream, position)
+                    task { return position }
+                | false, _, Some establishOrigin ->
+                    logger.Debug("Establishing position for stream {Stream}", stream, position)
+                    establishOrigin.Invoke cancellationToken
+                | false, _, None ->
+                    logger.Debug("Using initial position for stream {Stream}", stream)
+                    task { return Position.initial }
+
+            return struct (defaultCheckpointFrequency, position)
+        }

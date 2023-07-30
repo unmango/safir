696a1c95345e44d83e400a88cc84bf90871ed021
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-29 23:50:01 -0500

    Fiddling

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [

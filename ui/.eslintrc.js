8fe29b2bc3da9187941616b6a79bb01ece7742bc
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-30 17:16:43 -0500

    Configure angular eslint

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
+  ignorePatterns: [
+    "projects/**/*"
+  ],
   env: {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
@@ -16,4 +13,43 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {},
+  overrides: [
+    {
+      files: ["*.ts"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+        "prettier",
+      ],
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+      },
+    },
+    {
+      files: ["*.html"],
+      extends: [
+        "plugin:@angular-eslint/template/recommended",
+        "plugin:@angular-eslint/template/accessibility",
+        "prettier",
+      ],
+      rules: {},
+    },
+  ],
 };

7556fb6f8ddbaba507297aaecca6d558ea42e728
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-30 16:05:19 -0500

    Bootstrap with standalone APIs

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [SidebarComponent],
 })
 export class AppComponent {
   title = 'safir';
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,7 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, StoreModule.forRoot({}, {}), environment.imports)]
+})
   .catch(err => console.error(err));

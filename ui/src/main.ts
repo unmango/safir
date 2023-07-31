bf7798d38a583a94294c910f4eeacff5b030f08a
Erik Rasmussen <ecr.referee@hotmail.com>
2023-07-30 21:55:33 -0500

    Install ngrx effects

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideStore } from "@ngrx/store";
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideStore({}, {}),
     environment.providers,
-  ],
+    provideEffects()
+],
 }).catch((err) => console.error(err));

d52b21e27dc86329a749136c328724cbc4daae6f
Erik Rasmussen <ecr.referee@hotmail.com>
2023-08-01 00:02:37 -0500

    Add service config to environments

--- ui/src/environments/environment.development.ts
+++ ui/src/environments/environment.development.ts
@@ -5,4 +5,7 @@ export const environment = {
   providers: [
     provideStoreDevtools({ maxAge: 25 }),
   ],
+  service: {
+    baseUrl: 'http://localhost:5020'
+  }
 };
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   providers: [],
+  service: {
+    baseUrl: ''
+  }
 };

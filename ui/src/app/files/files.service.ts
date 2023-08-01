ee23d4d5a0f2acbfcb453c4971110875edff1ed5
Erik Rasmussen <ecr.referee@hotmail.com>
2023-08-01 00:03:37 -0500

    Implement the files service

--- ui/src/app/files/files.service.ts
+++ ui/src/app/files/files.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { createPromiseClient } from '@bufbuild/connect';
+import { createGrpcWebTransport } from '@bufbuild/connect-web';
+import { from, Observable } from 'rxjs';
+import { environment } from '../../environments';
+import * as connect from '../../gen/safir/v1alpha1/files_connect';
+import {
+  FilesServiceDiscoverRequest,
+  FilesServiceDiscoverResponse,
+  FilesServiceListResponse,
+} from '../../gen/safir/v1alpha1/files_pb';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilesService {
+  transport = createGrpcWebTransport({
+    baseUrl: environment.service.baseUrl,
+    useBinaryFormat: true,
+  });
 
-  constructor() { }
+  client = createPromiseClient(connect.FilesService, this.transport);
+
+  discover(req: FilesServiceDiscoverRequest): Observable<FilesServiceDiscoverResponse> {
+    return from(this.client.discover(req));
+  }
+
+  list(): Observable<FilesServiceListResponse> {
+    return from(this.client.list({}));
+  }
 }
